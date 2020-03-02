b9618ccaefc73883581ea86fc64396a45af1f7c2
JamesVera-Soto <jamesvs2020@gmail.com>
2020-03-02 14:02:24 -0600

    ui_experiment5 int()

--- lib/TaxonomyAbundance/TAUtils.py
+++ lib/TaxonomyAbundance/TAUtils.py
@@ -392,7 +392,7 @@ def get_mdf(attributeMappingId, category_name, callback_url, token):
     # Set metadata_samples
     metadata_samples = meta_dict.keys()
     # Make pandas DataFrame
-    mdf = pd.DataFrame(index=range(len(metadata_samples)), columns=['ID',category_name])
+    mdf = pd.DataFrame(index=range(len(metadata_samples)), columns=['ID', category_name])
     # Print sample and their grouping value
     i = 0
     for key, val in meta_dict.items():
@@ -410,9 +410,9 @@ def run(amp_id, row_attributes_id, attri_map_id, grouping_label, threshold, taxo
             mdf = get_mdf(attributeMappingId=attri_map_id, category_name=grouping_label, callback_url=callback_url, token=token)
             g1 = GraphData(df=df, mdf=mdf, scratch=scratch, callback_url=callback_url)
     except TypeError:
-        g1 = GraphData(df=df,mdf=pd.DataFrame(), scratch=scratch, callback_url=callback_url)
+        g1 = GraphData(df=df, mdf=pd.DataFrame(), scratch=scratch, callback_url=callback_url)
         grouping_label = ""
-    g1.graph_this(level=taxonomic_level, legend_font_size=8, cutoff=threshold, peek='all', category_field_name=grouping_label)
+    g1.graph_this(level=int(taxonomic_level), legend_font_size=8, cutoff=threshold, peek='all', category_field_name=grouping_label)
     return {
         'img_paths': g1.img_paths,
         'html_paths': g1.html_paths
