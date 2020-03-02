0f3ebf1c50b669629bce7b61ede9a81d3bd62055
JamesVera-Soto <jamesvs2020@gmail.com>
2020-03-02 14:15:49 -0600

    ui_experiment6 grouping_label['meta_group'][0]

--- lib/TaxonomyAbundance/TaxonomyAbundanceImpl.py
+++ lib/TaxonomyAbundance/TaxonomyAbundanceImpl.py
@@ -63,6 +63,7 @@ class TaxonomyAbundance:
         threshold = params.get('threshold')
         taxonomy_level = params.get('taxonomy_level')
         grouping_label = params.get('grouping_label')
+        grouping_label = grouping_label['meta_group'][0]
 
         csv_fp = "/kb/module/data/smalltx.csv"
         xls_fp = "/kb/module/data/moss_f50_metadata.xls"
